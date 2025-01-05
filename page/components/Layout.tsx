de0459f548397a5401bb8ae6de77a6dc344c3fbc
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-01-05 10:12:10 +0900

    feat: Layoutのstyle

--- page/components/Layout.tsx
+++ page/components/Layout.tsx
@@ -1,12 +1,27 @@
+import { Box, CssBaseline } from "@mui/material";
 import Footer from "./templates/Footer";
 import Header from "./templates/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div>
-			<Header />
-			<main>{children}</main>
-			<Footer />
-		</div>
+		<>
+			<CssBaseline />
+			<Box
+				component="div"
+				sx={{
+					display: "flex",
+					flexDirection: "column",
+					minHeight: "100dvh",
+					gap: "1rem",
+					p: 2,
+				}}
+			>
+				<Header />
+				<Box component="main" sx={{ flexGrow: 1 }}>
+					{children}
+				</Box>
+				<Footer />
+			</Box>
+		</>
 	);
 }

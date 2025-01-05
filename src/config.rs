38cbc65c34a92323ebbbaeabf877b65cd4526a42
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-01-06 02:21:26 +0900

    feat: crawlerにpassword認証を追加

--- src/config.rs
+++ src/config.rs
@@ -3,12 +3,17 @@ use std::env;
 #[derive(Clone)]
 pub struct Config {
     pub db_pool: sqlx::PgPool,
+    pub crawler_password: Option<String>,
 }
 
 impl Config {
     pub async fn load() -> Self {
         let database_url = env::var("DATABASE_URL").unwrap();
+        let crawler_password = env::var("CRAWLER_PASSWORD").ok();
         let db_pool = sqlx::PgPool::connect(&database_url).await.unwrap();
-        Self { db_pool }
+        Self {
+            db_pool,
+            crawler_password,
+        }
     }
 }

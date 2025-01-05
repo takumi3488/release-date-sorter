f43dc38bff8bc288ff497515194cb59e3788a02e
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-01-05 09:57:24 +0900

    feat: headerのstyle

--- page/components/templates/Header.tsx
+++ page/components/templates/Header.tsx
@@ -1,7 +1,15 @@
+import { Divider, Typography } from "@mui/material";
+import { Link } from "wouter";
+
 export default function Header() {
 	return (
 		<header>
-			<h1>発売日順に並び替えるやつ</h1>
+			<Link href="/" style={{ textDecoration: "none", color: "indigo" }}>
+				<Typography component="h1" variant="h3" fontFamily="Yuji Syuku">
+					発売日順に並び替えるやつ
+				</Typography>
+			</Link>
+			<Divider sx={{ pt: "1rem" }} />
 		</header>
 	);
 }

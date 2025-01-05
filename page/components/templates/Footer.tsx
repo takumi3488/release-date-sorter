e36753eb3f2ee838dd08ecd3a99829f95a0b893c
Takumi Mori <51111242+takumi3488@users.noreply.github.com>
2025-01-05 10:04:14 +0900

    style: lint

--- page/components/templates/Footer.tsx
+++ page/components/templates/Footer.tsx
@@ -4,7 +4,9 @@ export default function Footer() {
 	const year = new Date().getFullYear();
 	return (
 		<footer>
-			<Typography sx={{ textAlign: "center" }}>&copy; {year} Takumi Mori</Typography>
+			<Typography sx={{ textAlign: "center" }}>
+				&copy; {year} Takumi Mori
+			</Typography>
 		</footer>
 	);
 }
